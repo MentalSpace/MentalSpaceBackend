f8fd4d9b0b5e0d2a3a6919ad45035d584b63ac23
khmelraman <108018567+khmelraman@users.noreply.github.com>
2022-07-01 11:37:43 -0700

    Update Student.java

--- src/main/java/dev/mentalspace/wafflecone/Student.java
+++ src/main/java/dev/mentalspace/wafflecone/Student.java
@@ -10,16 +10,3 @@ public class Student {
 
     
 }
-
-/*
-        PreparedStatement getStudentFromDB = dbconn.prepareStatement("select * from student where student_id = ?");
-        getStudentFromDB.setInt(1, id);
-        ResultSet studentFromDB = getStudentFromDB.executeQuery();
-        studentFromDB.absolute(1);
-        student_id = studentFromDB.getLong(1);
-        cannonicalId = studentFromDB.getLong(2);
-        firstName = studentFromDB.getString(3);
-        lastName = studentFromDB.getString(4);
-        phone = studentFromDB.getInt(5);
-        grade = studentFromDB.getInt(6);
- */
