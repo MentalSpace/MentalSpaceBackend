15ab22199beb206a7d361dfbc724ae5b65b4a532
khmelraman <108018567+khmelraman@users.noreply.github.com>
2022-07-01 11:36:56 -0700

    Update Period.java

--- src/main/java/dev/mentalspace/wafflecone/Period.java
+++ src/main/java/dev/mentalspace/wafflecone/Period.java
@@ -11,17 +11,3 @@ public class Period {//period in java = class in db
 
     
 }
-
-/*
-        PreparedStatement getPeriodFromDB = dbconn.prepareStatement("select * from class where class_id = ?");
-        getPeriodFromDB.setInt(1, id);
-        ResultSet periodFromDB = getPeriodFromDB.executeQuery();
-        periodFromDB.absolute(1);
-        classId = periodFromDB.getLong(1);
-        teacherId = periodFromDB.getLong(2);
-        subjectId = periodFromDB.getLong(3);
-        termId = periodFromDB.getLong(4);
-        peirod = periodFromDB.getInt(5);
-        classCode = periodFromDB.getString(6);
-        archived = periodFromDB.getBoolean(7);
- */
