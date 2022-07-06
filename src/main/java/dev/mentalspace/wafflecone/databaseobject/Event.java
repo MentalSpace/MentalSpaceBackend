c56b7911f47ac20db6b68a3a29e4297d580efd29
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2022-07-06 11:11:21 -0700

    Update Event.java

--- src/main/java/dev/mentalspace/wafflecone/databaseobject/Event.java
+++ src/main/java/dev/mentalspace/wafflecone/databaseobject/Event.java
@@ -7,12 +7,7 @@ public class Event {
     public String description;
     public String rrule;
     public Long duration;
-    public RecurrenceRule recuringTime;
-
+    
     public Event() {
     }
-
-    public void setRecuringTime() {this.recuringTime = new RecurrenceRule(this.rrule);}
-
-    public long getStartTimeForTheDay(long day) {return (this.recuringTime.);}
 }
